850b6d1d70c7374986c838fa60fc95aeed7eb726
LauTsangFai <jayday@aliyun.com>
2020-12-03 13:04:48 +0800

    模拟电子时钟

---
+++ AnalogClock/main.js
@@ -0,0 +1,20 @@
+const hourHand = document.querySelector(".hour");
+const minuteHand = document.querySelector(".minute");
+const secondHand = document.querySelector(".second");
+console.log(secondHand);
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty("--rotation", `${rotationRatio * 360}`);
+}
+
+setClock();
+setInterval(setClock, 1000);
